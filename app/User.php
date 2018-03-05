303c6f615b216afa91e62ccb1322f7978f495b31
Mayaka Donnicias <mayakadonnicias@gmail.com>
2018-03-05 15:13:30 +0300

    Default max string length set.Employee and user migrations done. User and Employee model done.Relation mapping user to employee detail done.

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function Employee_Record(){
+        return $this->hasOne("App\Employee");
+    }
+
 }
