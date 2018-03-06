d6d74de13b6697bee468e351ea3d4d8bb5c9bc18
Michael <michael.kamau@kinetics.co.ke>
2018-03-06 14:35:53 +0300

    Break Inspinia theme into components, Created login, password reset and dashboard views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Reset Password</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+                <h2 class="logo-name">NH</h2>
             </div>
+            <h3>Reset Password</h3>
+            {{--<p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.--}}
+            {{--<!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->--}}
+            {{--</p>--}}
+            <p>Send password reset link to my email</p>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <form class="m-t" role="form"method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="form-group">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback">
+                                <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary block full-width m-b">Send Password Reset Link</button>
+                {{--<a  href="{{ route('password.request') }}"><small>Forgot password?</small></a>--}}
+                {{--<p class="text-muted text-center"><small>Do not have an account?</small></p>--}}
+                {{--<a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>--}}
+            </form>
+            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
         </div>
     </div>
-</div>
 @endsection
