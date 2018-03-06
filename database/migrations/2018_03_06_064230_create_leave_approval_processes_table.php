864647092f01cb29e199a9034a289ed8263729cc
Mayaka Donnicias <mayakadonnicias@gmail.com>
2018-03-06 09:50:24 +0300

    Added leave approval process entries migration and model

---
+++ database/migrations/2018_03_06_064230_create_leave_approval_processes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaveApprovalProcessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_approval_processes', function (Blueprint $table) {
+            $table->increments('approval_process_id');
+            $table->integer('Application_Code',false,true);
+            $table->string('leave_type',20);
+            $table->decimal('Approved_days');
+            $table->date('Approved_Start_Date');
+            $table->date('Approved_End_Date');
+            $table->string('Comments','250');
+            $table->integer('Approver_id',false,true);
+            $table->Integer('Sequence_No');
+            $table->foreign("Approver_id")->references("Approver_id")->on("employee_approvers")->onDelete('cascade');
+            $table->foreign("Application_Code")->references("Application_Code")->on("employee_leave_applications")->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_approval_processes');
+    }
+}
