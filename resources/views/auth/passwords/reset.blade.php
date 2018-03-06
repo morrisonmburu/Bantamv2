9042c9ad6e1dd2e02bbcc674467c23b4fa58d902
Michael <michael.kamau@kinetics.co.ke>
2018-03-06 14:35:53 +0300

    Break Inspinia theme into components, Created login, password reset and dashboard views

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -68,3 +68,60 @@
     </div>
 </div>
 @endsection
+
+
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+                <h2 class="logo-name">NH</h2>
+            </div>
+            <h3>Welcome to Bantam</h3>
+            {{--<p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.--}}
+            {{--<!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->--}}
+            {{--</p>--}}
+            <p>Login in. To see it in action.</p>
+            <form class="m-t" role="form" method="POST" action="{{ route('password.request') }}">
+                @csrf
+
+                <div class="form-group">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email or old('email') }}" required autofocus>
+
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <input id="password-confirm" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" required>
+
+                    @if ($errors->has('password_confirmation'))
+                        <span class="invalid-feedback">
+                                <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary block full-width m-b">Reset Password</button>
+                {{--<a  href="{{ route('password.request') }}"><small>Forgot password?</small></a>--}}
+                {{--<p class="text-muted text-center"><small>Do not have an account?</small></p>--}}
+                {{--<a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>--}}
+            </form>
+            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/dashboard/page_wrapper_components/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="footer">
+
+    {{--Right content--}}
+    <div class="pull-right">
+        10GB of <strong>250GB</strong> Free.
+    </div>
+
+    {{--Left Content--}}
+    <div>
+        <strong>Copyright</strong> Example Company &copy; 2014-2017
+    </div>
+</div>
