4064afd7a12a86b19e305bc63e4aaa500f31b035
Michael <michael.kamau@kinetics.co.ke>
2018-03-06 14:35:53 +0300

    Break Inspinia theme into components, Created login, password reset and dashboard views

---
+++ resources/views/layouts/dashboard/side_nav_components/dropdown_link.blade.php
@@ -0,0 +1,11 @@
+<li>
+    <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
+    {{--<a href="#"><i class="fa fa-globe"></i> <span class="nav-label">Miscellaneous</span><span class="label label-info pull-right">NEW</span></a>--}}
+    <ul class="nav nav-second-level collapse">
+        <li><a href="index.html">Dashboard v.1</a></li>
+        <li><a href="dashboard_2.html">Dashboard v.2</a></li>
+        <li><a href="dashboard_3.html">Dashboard v.3</a></li>
+        <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
+        <li><a href="dashboard_5.html">Dashboard v.5 </a></li>
+    </ul>
+</li>
