6a83eacb00d277adae901f4b73c79bc97f943dd2
Mayaka Donnicias <mayakadonnicias@gmail.com>
2018-03-06 16:41:40 +0300

    Defaults set to flags and sync timestamps

--- database/migrations/2018_03_05_123925_create_leave_types_table.php
+++ database/migrations/2018_03_05_123925_create_leave_types_table.php
@@ -23,10 +23,10 @@ class CreateLeaveTypesTable extends Migration
             $table->boolean('Inclusive_of_non_working');
             $table->boolean('allow_inactive');
             $table->timestamps();
-            $table->boolean("Nav_Synced");
-            $table->boolean("Web_Synced");
-            $table->dateTime("Last_Nav_Synced");
-            $table->dateTime("Last_Web_Synced");
+            $table->boolean("Nav_Synced")->default(false);
+            $table->boolean("Web_Synced")->default(true);
+            $table->dateTime("Last_Nav_Synced")->nullable();
+            $table->dateTime("Last_Web_Synced")->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
