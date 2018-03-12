2db05a6cf7bfe9e8afdbd5de21828d4d20ccb0b0
Mayaka Donnicias <mayakadonnicias@gmail.com>
2018-03-12 15:51:35 +0300

    Added approval requests fetching per employee

--- database/migrations/2018_03_05_131608_create_employee_approvers_table.php
+++ database/migrations/2018_03_05_131608_create_employee_approvers_table.php
@@ -24,12 +24,10 @@ class CreateEmployeeApproversTable extends Migration
             $table->foreign('Employee')->references('No')->on('employees');
             $table->foreign('Approver')->references('No')->on('employees');
             $table->unique(['Employee', 'Approver', 'Approval_Level']);
-
             $table->boolean("Nav_Sync")->default(false);
             $table->boolean("Web_Sync")->default(true);
             $table->dateTime("Nav_Sync_TimeStamp")->nullable();
             $table->dateTime("Web_Sync_TimeStamp")->default(DB::raw('CURRENT_TIMESTAMP'));
-
             $table->timestamps();
         });
     }
