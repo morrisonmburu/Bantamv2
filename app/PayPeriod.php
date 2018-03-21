271b6758ac944b50c3ce23563a678c1be373865b
Brian Keyonzo <briankeyonzo@yahoo.com>
2018-03-21 11:15:54 +0300

    Updated

--- app/PayPeriod.php
+++ app/PayPeriod.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class PayPeriod extends Model
 {
-    protected $guarded = [];
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->fillable = DB::getSchemaBuilder()->getColumnListing($this->table);
+    }
 }
